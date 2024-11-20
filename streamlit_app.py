d2572c0db52386ddf3b184ffe3a15e4e3da1f653
umeno-noa <umeno.noa.5027@members.co.jp>
2024-11-20 10:32:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # SnowparkデータフレームをPandasデータフレームに変換して、LOC関数を使用できるようにする
 pd_df=my_dataframe. to_pandas()
-st. dataframe (pd_df)
-st.stop()
+#st. dataframe (pd_df)
+#st.stop()
 
 
 #ユーザーが複数の果物（スムージーの材料）を選択できるように、multiselect ウィジェットを追加
@@ -42,10 +42,14 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+            ingredients_string += fruit_chosen + ' '
+
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+            st.subheader(fruit_chosen + 'Nutrition Information')
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #smoothies.public.orders テーブルの ingredients カラムに、選択された果物の名前を挿入する
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
